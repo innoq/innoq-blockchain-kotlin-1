657a3576e60b8c9de1ecb276c1bc7acd0ff13893
Jochen Christ <jochen.christ@innoq.com>
2018-04-19 16:40:19 +0200

    Add unit tests.

--- src/main/kotlin/Miner.kt
+++ src/main/kotlin/Miner.kt
@@ -2,19 +2,26 @@ import com.google.gson.Gson
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 
-class Miner {
+object Miner {
     private val digest = MessageDigest.getInstance("SHA-256")!!
-    private fun hashBlock(block: Block): String {
+
+    fun hashBlock(block: Block): String {
         return digest.digest(Gson().toJson(block).toByteArray(StandardCharsets.UTF_8)).toHex()
     }
 
     fun mine(head: Block): Block {
+        return mine(head, System.currentTimeMillis())
+    }
+
+    fun mine(head: Block, timestamp: Long): Block {
         val headHash = hashBlock(head)
-        val block = Block(head.index + 1, System.currentTimeMillis(), 0, listOf(), headHash)
+        val block = Block(head.index + 1, timestamp, 0, listOf(), headHash)
         do {
             ++block.proof
             val hash = hashBlock(block)
         } while (!hash.startsWith("000"))
         return block
     }
+
+
 }
---
+++ src/test/kotlin/MinerTest.kt
@@ -0,0 +1,33 @@
+import com.google.gson.Gson
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MinerTest {
+
+    @Test
+    fun `should mine new block on genesis`() {
+        // given
+        val genesisBlockString = "{\"index\":1,\"timestamp\":0,\"proof\":1917336,\"transactions\":[{\"id\":\"b3c973e2-db05-4eb5-9668-3e81c7389a6d\",\"timestamp\":0,\"payload\":\"I am Heribert Innoq\"}],\"previousBlockHash\":\"0\"}"
+        val genesisBlock = Gson().fromJson<Block>(genesisBlockString, Block::class.java)
+
+        // when
+        val block = Miner.mine(genesisBlock, 324345345)
+
+        // then
+        assertEquals("""00096CB02B4D30119EB874FB79AC77F4DEDD76D5965A6ACBEB2ACBF0C2DE5260""", Miner.hashBlock(block))
+    }
+
+    @Test
+    fun `hash should have leading zeros`() {
+        // given
+        val genesisBlockString = "{\"index\":1,\"timestamp\":0,\"proof\":1917336,\"transactions\":[{\"id\":\"b3c973e2-db05-4eb5-9668-3e81c7389a6d\",\"timestamp\":0,\"payload\":\"I am Heribert Innoq\"}],\"previousBlockHash\":\"0\"}"
+        val genesisBlock = Gson().fromJson<Block>(genesisBlockString, Block::class.java)
+
+        // when
+        val block = Miner.mine(genesisBlock)
+
+        // then
+        assertTrue(Miner.hashBlock(block).startsWith("000"))
+    }
+}
