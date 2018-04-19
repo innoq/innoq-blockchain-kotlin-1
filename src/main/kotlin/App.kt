94d089b9eff71f5798b82f29e5dd5a721e205b36
felix <felix.schumacher@innoq.com>
2018-04-19 12:28:44 +0200

    genesis block als string eingefügt

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,8 @@
+val genesisBlockString = "{\"index\": 1,\"timestamp\": 0,\"proof\": 955977,\"transactions\":" +
+        " [{\"id\": \"b3c973e2-db05-4eb5-9668-3e81c7389a6d\",\"timestamp\": 0,\"payload\":" +
+        " \"I am Heribert Innoq\"  }],\"previousBlockHash\": \"0\"}";
+
+
 fun main(args: Array<String>) {
     println("Hallo Blockchain")
 }
