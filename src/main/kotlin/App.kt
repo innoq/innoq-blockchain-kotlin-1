30d1567b23d1c0cb73d442e7d9321e3d8b767527
Martin Petras <martin.petras@innoq.com>
2018-04-19 14:54:19 +0200

    Basic web routes

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -9,7 +9,8 @@ import io.ktor.server.netty.*
 
 val genesisBlockString = "{\"index\":1,\"timestamp\":0,\"proof\":1917336,\"transactions\":[{\"id\":\"b3c973e2-db05-4eb5-9668-3e81c7389a6d\",\"timestamp\":0,\"payload\":\"I am Heribert Innoq\"}],\"previousBlockHash\":\"0\"}";
 
-data class Person(val lastname: String, val firstname: String)
+
+data class MiningResponse(val message: String, val block: Block)
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
@@ -20,10 +21,13 @@ fun main(args: Array<String>) {
         }
         routing {
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                call.respond(Node("", 1))
             }
-            get("/test") {
-                call.respond(Person("Doe", "John"))
+            get("/blocks") {
+                call.respond(Chain(listOf(Block(1, 0, 0, listOf(), "0"))))
+            }
+            get("/mine") {
+                call.respond(MiningResponse("Mined ...", Block(1, 0, 0, listOf(), "0")))
             }
         }
     }
