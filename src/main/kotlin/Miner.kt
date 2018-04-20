f02589f6529b8354705f0eff2b207de4cda011ab
Jochen Christ <jochen.christ@innoq.com>
2018-04-20 09:38:03 +0200

    hash as extension function

--- src/main/kotlin/Miner.kt
+++ src/main/kotlin/Miner.kt
@@ -1,3 +1,6 @@
+import java.util.stream.LongStream
+import java.util.stream.Stream
+
 object Miner {
 
     fun mine(head: Block): MiningResponse {
@@ -8,12 +11,24 @@ object Miner {
         var hashes : Long =0
         val start = System.currentTimeMillis()
         val headHash = head.hash()
-        val block = Block(head.index + 1, timestamp, 0, listOf(), headHash)
-        do {
-            ++block.proof
-            val hash = block.hash()
-            hashes++
-        } while (!hash.startsWith("000"))
+
+//        val block = Block(head.index + 1, timestamp, 0, listOf(), headHash)
+//
+//        do {
+//            ++block.proof
+//            val hash = block.hash()
+//            hashes++
+//        } while (!hash.startsWith("000"))
+
+        val index = head.index + 1
+
+        val block = Stream.iterate(0L, {i -> hashes=i; i+1})
+                .map { Block(index, timestamp, it, listOf(), headHash) }
+                .filter{ it.hash().startsWith("00000") }
+                .findFirst()
+                .get()
+
+
         val stop =System.currentTimeMillis()
         val seconds = (stop.toDouble()-start.toDouble())/1000.toDouble()
         val hashesPerSecond= hashes/seconds
