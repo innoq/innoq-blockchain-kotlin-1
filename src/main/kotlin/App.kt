e326bc95b4303e36f355091e6f928abbdb2172ed
Jochen Christ <jochen.christ@innoq.com>
2018-04-20 12:10:36 +0200

    Fix 406 http client responses. Add logging. Change to 8333

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,13 +1,17 @@
-import io.ktor.application.*
+import com.google.gson.Gson
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import com.google.gson.*
 import io.ktor.request.receive
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import java.security.MessageDigest
 import java.util.*
 
@@ -31,6 +35,8 @@ fun Application.blockChain(){
             //setPrettyPrinting()
         }
     }
+    install(CallLogging)
+
     routing {
         get("/") {
             call.respond(Node(uuid, chain.blockHeight))
@@ -58,6 +64,6 @@ fun Application.blockChain(){
 data class TxRequest(val payload: String)
 
 fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, port = 8089, module = Application::blockChain)
+    val server = embeddedServer(Netty, port = 8333, module = Application::blockChain)
     server.start(wait = true)
 }
